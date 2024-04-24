69213f2907cc9d412033c36e69b2b465cd6d7997
Canoe10 <113536800+Canoe10@users.noreply.github.com>
2024-04-24 14:47:27 -0500

    Fixed compilation issue in movement.c

--- godzillaHunter/movement.h
+++ godzillaHunter/movement.h
@@ -2,20 +2,34 @@
 #define MOVEMENT_H
 
 #define MOVEOFFSET 0
-#define TURNOFFSET 10 // - for undershoot + for overshoot degrees off of correct turn
+#define TURNOFFSET 10 // - for undershoot + for overshoot degrees off of correct tur
 
-#define TWISTOFFSET -5 //positive is clockwise driving forward
+#define TWISTOFFSET -1 //positive is clockwise driving forward
+
 
 #include "open_interface.h"
 #include "button.h"
+#include "Timer.h"
+#include <math.h>
 
-double moveCalibrate(oi_t *sensor_data);
-double turnCalibrate(oi_t *sensor_data);
+#define DEGREES_TO_RADS M_PI / 180.0
 
-double move_forward(oi_t *sensor_data, double distance_mm);
-double move_backward(oi_t *sensor_data, double distance_mm);
-double turn_right(oi_t *sensor, double degrees);
-double turn_left(oi_t *sensor, double degrees);
-double ram(oi_t *sensor);
+typedef struct {
+    double x;
+    double y;
+    double heading;
+} coords;
+
+double moveCalibrate(oi_t *sensor_data, coords *robotCoords);
+double turnCalibrate(oi_t *sensor_data, coords *robotCoords);
+
+double move_forward(oi_t *sensor_data, coords *robotCoords, double distance_mm);
+double move_to_point(oi_t *sensor_data, coords *robotCoords, double global_x, double global_y);
+double move_backward(oi_t *sensor_data, coords *robotCoords, double distance_mm);
+double turn_right(oi_t *sensor, coords *robotCoords, double degrees);
+double turn_left(oi_t *sensor, coords *robotCoords, double degrees);
+double ram(oi_t *sensor_data, coords *robotCoords);
+
+void manuever(oi_t *sensor_data, float distance_mm, coords *robotCoords);
 
 #endif
