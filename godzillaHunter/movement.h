3c2e7ff59d8fabeacc15d8fc2b1685b60c8dc658
Schulmeister <jdschul5@iastate.edu>
2024-04-26 01:12:24 -0500

    Made it avoid an example object

--- godzillaHunter/movement.h
+++ godzillaHunter/movement.h
@@ -2,9 +2,9 @@
 #define MOVEMENT_H
 
 #define MOVEOFFSET 0
-#define TURNOFFSET 10 // - for undershoot + for overshoot degrees off of correct tur
+#define TURNOFFSET 14 // - for undershoot + for overshoot degrees off of correct tur
 
-#define TWISTOFFSET -6 //positive is clockwise driving forward
+#define TWISTOFFSET -7 //positive is clockwise driving forward
 
 
 #include "open_interface.h"
@@ -17,32 +17,35 @@
 #define AVOID_DISTANCE 200
 
 typedef struct {
-    double x;
-    double y;
-    double heading;
+    float x;
+    float y;
+    float heading;
 } coords;
 
 typedef struct
 {
-    double x;
-    double y;
-    double linearWidth;
+    float x;
+    float y;
+    float linearWidth;
 }object;
 
-double moveCalibrate(oi_t *sensor_data, coords *robotCoords);
-double turnCalibrate(oi_t *sensor_data, coords *robotCoords);
 
-double move_forward(oi_t *sensor_data, coords *robotCoords, double distance_mm);
-double move_to_point(oi_t *sensor_data, coords *robotCoords, object *obs, double global_x, double global_y);
-double checkObstacles(oi_t *sensor_data, coords *robotCoords, object *obs, double global_x, double global_y);
-coords calculatePerpendicularPoint(coords robotCoords, coords targetCoords);
-int compareDistances(const void *a, const void *b)
-double calcDistToRobot(coords *robotCoords, object *obs);
-double calcDistToPath(coords *robotCoords, object *obs, double global_x, double global_y);
-double move_backward(oi_t *sensor_data, coords *robotCoords, double distance_mm);
-double turn_right(oi_t *sensor, coords *robotCoords, double degrees);
-double turn_left(oi_t *sensor, coords *robotCoords, double degrees);
-double ram(oi_t *sensor, coords *robotCoords);
+char toPutty[50];
+
+float moveCalibrate(oi_t *sensor_data, coords *robotCoords);
+float turnCalibrate(oi_t *sensor_data, coords *robotCoords);
+
+float move_forward(oi_t *sensor_data, coords *robotCoords, float distance_mm);
+float move_to_point(oi_t *sensor_data, coords *robotCoords, object *obs, float global_x, float global_y);
+float checkObstacles(oi_t *sensor_data, coords *robotCoords, object *obs, float global_x, float global_y);
+coords calculatePerpendicularPoint(coords *robotCoords, object targetCoords);
+int compareDistances(const void *a, const void *b);
+float calcDistToRobot(coords *robotCoords, object *obs);
+float calcDistToPath(coords *robotCoords, object *obs, float global_x, float global_y);
+float move_backward(oi_t *sensor_data, coords *robotCoords, float distance_mm);
+float turn_right(oi_t *sensor, coords *robotCoords, float degrees);
+float turn_left(oi_t *sensor, coords *robotCoords, float degrees);
+float ram(oi_t *sensor, coords *robotCoords);
 
 void manuever(oi_t *sensor_data, coords *robotCoords, float distance_mm);
 
