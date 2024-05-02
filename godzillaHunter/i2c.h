6d2911a7680d05eea877ffd82abe25d349b5b7f2
Parker Pederson <parkerp0@iastate.edu>
2024-05-02 13:58:52 -0500

    added i2c in godzillaHunter

---
+++ godzillaHunter/i2c.h
@@ -0,0 +1,8 @@
+#ifndef I2C_H
+#define I2C_H
+
+#include <inc/tm4c123gh6pm.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#endif
---
+++ godzillaHunter/imu.h
@@ -0,0 +1,6 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include "i2c.h"
+
+#endif
